82e32be3c74443f8b45a184fcc63ae093324a546
Chad Parker <chad@chadparker.com>
2020-01-27 20:41:10 -0800

    Short-as-possible personal functional challenge

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -27,3 +27,21 @@ print(numberOfVowels(in: "Polly wants a cracker!", isYAVowel: true))
 print(numberOfVowels(in: "POLLY WANTS A CRACKER!", isYAVowel: true))
 print(numberOfVowels(in: "Rhythm"))
 print(numberOfVowels(in: "Rhythm", isYAVowel: true))
+
+
+
+// my short-as-possible functional challenge:
+
+func numberOfVowels2(in string: String, isYAVowel: Bool = false) -> Int {
+    var vowels = ["a", "e", "i", "o", "u"]
+    if isYAVowel { vowels.append("y")}
+    return string.lowercased().filter { vowels.contains(String($0)) }.count
+}
+
+print("=====")
+print(numberOfVowels2(in: "Polly wants a cracker!"))
+print(numberOfVowels2(in: "POLLY WANTS A CRACKER!"))
+print(numberOfVowels2(in: "Polly wants a cracker!", isYAVowel: true))
+print(numberOfVowels2(in: "POLLY WANTS A CRACKER!", isYAVowel: true))
+print(numberOfVowels2(in: "Rhythm"))
+print(numberOfVowels2(in: "Rhythm", isYAVowel: true))
