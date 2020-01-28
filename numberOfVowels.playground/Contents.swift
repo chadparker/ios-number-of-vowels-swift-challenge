6a388335efcd621a93e564b5e2f24826ecb647ab
Chad Parker <chad@chadparker.com>
2020-01-27 20:26:08 -0800

    Project complete

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
 
+// need to loop through string, counting vowels
+// case-insensitive, so lowercase() first
+
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    let lowercaseString = string.lowercased()
+    var vowelCount = 0
+    for character in lowercaseString {
+        switch character {
+        case "a", "e", "i", "o", "u":
+            vowelCount += 1
+        case "y":
+            if isYAVowel {
+                vowelCount += 1
+            }
+        default:
+            break
+        }
+    }
+    return vowelCount
 }
+
+print(numberOfVowels(in: "Polly wants a cracker!"))
+print(numberOfVowels(in: "POLLY WANTS A CRACKER!"))
+print(numberOfVowels(in: "Polly wants a cracker!", isYAVowel: true))
+print(numberOfVowels(in: "POLLY WANTS A CRACKER!", isYAVowel: true))
+print(numberOfVowels(in: "Rhythm"))
+print(numberOfVowels(in: "Rhythm", isYAVowel: true))
